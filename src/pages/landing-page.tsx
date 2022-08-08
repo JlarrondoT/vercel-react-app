bf5eee04f1010429a7dab596dbb897d815912c4a
Juan Carlos Larrondo <juancarlos1221@gmail.com>
2022-08-08 00:26:57 -0400

    first commit

---
+++ src/pages/landing-page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './landing-page.css';
+
+function LandingPage() {
+  return (
+    <div className="landing-container">
+        <p>esto es un ejemplo</p>
+    </div>
+  );
+}
+
+export default LandingPage;
